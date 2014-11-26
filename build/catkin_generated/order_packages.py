e5abab2ba2196fac9bd8683304d386b8ec70facb
George Lydakis <ldkge@CLUVINSIEPM.(none)>
2014-11-26 16:04:35 +0200

    Initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ldkge/Documents/git/inemo_node/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/hydro".split(';') if "/opt/ros/hydro" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/ldkge/Documents/git/inemo_node/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/ldkge/Documents/git/inemo_node/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ldkge/Documents/git/inemo_node/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/ldkge/Documents/git/inemo_node/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/ldkge/Documents/git/inemo_node/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/ldkge/Documents/git/inemo_node/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/ldkge/Documents/git/inemo_node/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/ldkge/Documents/git/inemo_node/build"
+export PYTHONPATH="/home/ldkge/Documents/git/inemo_node/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ldkge/Documents/git/inemo_node/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ldkge/Documents/git/inemo_node/src:$ROS_PACKAGE_PATH"
