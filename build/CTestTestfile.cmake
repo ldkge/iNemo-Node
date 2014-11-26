731e969ca35bd929462575eab09fb7796bdb2c86
George Lydakis <ldkge@CLUVINSIEPM.(none)>
2014-11-26 16:37:56 +0200

    Removed Myzhar repo and also build and devel folders

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ldkge/Documents/git/inemo_node/src
-# Build directory: /home/ldkge/Documents/git/inemo_node/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
